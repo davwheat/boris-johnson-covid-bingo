0d40d3d10152bc57a51c57de29e28bb3d1a5dc3c
David Wheatley <hi@davwheat.dev>
2021-01-06 03:38:31 +0000

    Add SEO data to html.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,56 +10,14 @@ import PropTypes from 'prop-types'
 import { Meta, Title } from 'react-head'
 
 function SEO({ description, meta, title }) {
-  const metaDescription =
-    description ||
-    "Do you find Boris Johnson's Coronavirus briefings ridiculously dull? Play a game of BoJo Bingo: mark off each phrase and action as they happen in real time."
+  // const metaDescription =
+  //   description ||
+  //   "Do you find Boris Johnson's Coronavirus briefings ridiculously dull? Play a game of BoJo Bingo: mark off each phrase and action as they happen in real time."
   const defaultTitle = 'BoJo Briefing Bingo'
-
-  const defaultMeta = [
-    {
-      name: `description`,
-      content: metaDescription,
-    },
-    {
-      property: `og:title`,
-      content: title,
-    },
-    {
-      property: `og:description`,
-      content: metaDescription,
-    },
-    {
-      property: `og:type`,
-      content: `website`,
-    },
-    {
-      name: `twitter:card`,
-      content: `summary`,
-    },
-    // {
-    //   name: `twitter:creator`,
-    //   content: site.siteMetadata?.author || ``,
-    // },
-    {
-      name: `twitter:title`,
-      content: title,
-    },
-    {
-      name: `twitter:description`,
-      content: metaDescription,
-    },
-  ]
-
-  let finalMeta = defaultMeta.concat(meta)
 
   return (
     <>
       <Title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</Title>
-
-      {/* Add meta tags */}
-      {finalMeta.map(mObj => (
-        <Meta key={mObj.name + mObj.content} name={mObj.name} content={mObj.content} />
-      ))}
     </>
   )
 }
--- src/html.js
+++ src/html.js
@@ -8,6 +8,41 @@ export default function HTML(props) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        {/* <!-- Search Engine --> */}
+        <meta
+          name="description"
+          content="Do you find Boris Johnson's Coronavirus briefings ridiculously dull? Play a game of BoJo Bingo: mark off each phrase and action as they happen in real time."
+        />
+        <meta name="image" content="https://borisbingo.com/promo.jpg" />
+        {/* <!-- Schema.org for Google --> */}
+        <meta itemprop="name" content="BoJo Briefing Bingo" />
+        <meta
+          itemprop="description"
+          content="Do you find Boris Johnson's Coronavirus briefings ridiculously dull? Play a game of BoJo Bingo: mark off each phrase and action as they happen in real time."
+        />
+        <meta itemprop="image" content="https://borisbingo.com/promo.jpg" />
+        {/* <!-- Twitter --> */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="BoJo Briefing Bingo" />
+        <meta
+          name="twitter:description"
+          content="Do you find Boris Johnson's Coronavirus briefings ridiculously dull? Play a game of BoJo Bingo: mark off each phrase and action as they happen in real time."
+        />
+        <meta name="twitter:site" content="@davwheat_" />
+        <meta name="twitter:creator" content="@davwheat_" />
+        <meta name="twitter:image:src" content="https://borisbingo.com/promo.jpg" />
+        {/* <!-- Open Graph general (Facebook, Pinterest & Google+) --> */}
+        <meta name="og:title" content="BoJo Briefing Bingo" />
+        <meta
+          name="og:description"
+          content="Do you find Boris Johnson's Coronavirus briefings ridiculously dull? Play a game of BoJo Bingo: mark off each phrase and action as they happen in real time."
+        />
+        <meta name="og:image" content="https://borisbingo.com/site-ss.png" />
+        <meta name="og:url" content="https://borisbingo.com/" />
+        <meta name="og:locale" content="en_GB" />
+        <meta name="og:type" content="website" />
+
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
