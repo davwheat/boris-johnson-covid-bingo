7c7ca6a65de8a0534e20e5fc8fef5715d976d724
David Wheatley <hi@davwheat.dev>
2021-01-09 03:05:05 +0000

    Deploy website

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4b09f15ed71a09677444.js"
+    "url": "webpack-runtime-da1914de48de53d4f418.js"
   },
   {
     "url": "styles.ba7078458045f7aa6de1.css"
@@ -47,7 +47,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0ee0992b01de88695749918ae0de9c98"
+    "revision": "1d676b16c99c112e79f48e554e764ab6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-258fa972005c75a2a109.js"
@@ -58,7 +58,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "eef9315bdc37efd9d9874d382b3a04a2"
+    "revision": "af5e40a722ea13aabe7782fc2bd6bf39"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
