f611ea2953a87cedf58df32e9c948b861803c0ab
David Wheatley <hi@davwheat.dev>
2021-01-09 03:05:02 +0000

    Add support for custom AlertBanner theme

--- src/components/AlertBanner.js
+++ src/components/AlertBanner.js
@@ -26,17 +26,28 @@ const useStyles = makeStyles({
   },
 })
 
-export default function AlertBanner({ title, message, noPadding, afterHeading, alternateColor, innerClassName }) {
+/**
+ * A simple alert banner.
+ *
+ * @param {object} props
+ * @param {string} props.title The alert's title
+ * @param {string} props.message The alert's message
+ * @param {boolean} [props.afterHeading] Removes top padding
+ * @param {boolean} [props.alternateBackground] Whether to use the alternate dark background
+ * @param {string} [props.innerClassName]
+ * @param {{ background: string, color: string }} [props.customTheme] A custom theme for the alert
+ *
+ * @export
+ * @return {React.ReactNode}
+ */
+export default function AlertBanner(props) {
+  const { title, message, noPadding, afterHeading, alternateColor, customTheme, innerClassName } = props
   const classes = useStyles()
 
   return (
     <section
-      className={clsx(
-        classes.root,
-        !noPadding && classes.padBottom,
-        afterHeading && classes.afterHeading,
-        alternateColor && classes.alternateBackground,
-      )}
+      className={clsx(classes.root, !noPadding && classes.padBottom, afterHeading && classes.afterHeading, alternateColor && classes.alternateBackground)}
+      style={{ background: customTheme.background, color: customTheme.color }}
     >
       <div className={innerClassName}>
         <Paragraph className={classes.title} bold>
